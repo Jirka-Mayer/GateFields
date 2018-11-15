5e9832cc4c3e1ec2d726e91f9512cb42bd887865
Jirka-Mayer <jirkamayer97@gmail.com>
2018-11-15 09:33:41 +0100

    Setup testing

---
+++ src/test/cs/jirkamayer/gatefields/CameraTest.java
@@ -0,0 +1,13 @@
+package cs.jirkamayer.gatefields;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CameraTest {
+
+    @Test
+    void worldToScreen() {
+        assertTrue(true);
+    }
+}
